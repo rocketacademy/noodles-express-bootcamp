a75d82d888c00603066b3f46523cc7b62d44b6b3
Kimmi Lee <lee.kimmixq@gmail.com>
2021-10-25 12:44:13 +0900

    completed base

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('hello world');
+import express from 'express';
+import { read } from './jsonFileStorage.js';
+
+const app = express();
+
+const handleRecipeReq = (req, res) => {
+  console.log(`received req for recipe index ${req.params.index}`);
+  let content = '';
+  read('data.json', (err, { recipes }) => {
+    if (!err) {
+      // server error checking
+      if (recipes[req.params.index] === undefined) {
+        console.log(`cannot find recipe index ${req.params.index}`)
+        res.status(404).send('Sorry, we cannot find that!');
+        return;
+      }
+      // get recipe by index
+      content = recipes[req.params.index];
+        console.log(`sending res for recipe index ${req.params.index}`);
+        res.send(content);
+    }
+  });
+};
+
+app.get('/recipe/:index', handleRecipeReq);
+
+app.listen(3004);
