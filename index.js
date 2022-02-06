63a1d5b4151e8cfbdba116d4da642e32dad16486
Hertanto Irawan <hertanto.irawan@outlook.com>
2022-02-07 00:02:04 +0700

    Base

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+import express from 'express';
+import { read } from './jsonFileStorage.js';
+
+const app = express();
+
+// default request
+const handleIncomingRequest = (req, res) => {
+  if (isNaN(req.params.index)) {
+    res.status(404).send('Sorry, we cannot find that!');
+    return;
+  }
+  read('data.json', (err, data) => {
+    res.send(data.recipes[req.params.index]);
+  });
+};
+app.get('/recipe/:index', handleIncomingRequest);
+
+app.listen(3004);
