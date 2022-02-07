5e7d81c8d0315eee4009e3cb7cf272d41aa935b2
Goh Yong Siang <gysiang@gmail.com>
2022-02-07 20:13:44 +0800

    first commit - base

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('hello world');
+import express from 'express';
+import { read } from './jsonFileStorage.js';
+
+const PORT = 3004;
+
+const app = express();
+
+const whenIncomingRequestIndex = (req, res) => {
+  console.log('request came in ');
+
+  read('data.json', (data, error) => {
+    if (error) {
+      console.log('read error', error);
+    }
+
+    const indexOfRecipe  = req.params.index;
+
+    const recipe = data.recipes[indexOfRecipe];
+
+    if (recipe) {
+      res.send(recipe);
+    } else {
+      res.status(404).send('sorry, we cannot find that!');
+    }
+  });
+};
+
+app.get('/recipe/:index', whenIncomingRequestIndex);
+app.listen(PORT);
