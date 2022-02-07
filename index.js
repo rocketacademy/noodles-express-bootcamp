c2be8b13d90b8725c96a600c2758b78099602a8e
Goh Yong Siang <gysiang@gmail.com>
2022-02-07 23:58:15 +0800

    get recipe by yield

--- index.js
+++ index.js
@@ -25,5 +25,24 @@ const whenIncomingRequestIndex = (req, res) => {
   });
 };
 
+const whenIncomingRequestYield = (request, response) => {
+  console.log('request came in ');
+
+   read('data.json', (data, error) => {
+
+    if (error) {
+      console.log('read error', error);
+    }
+
+    const portion = parseInt(request.params.portion, 10);
+
+    const recipes = data.recipes.filter((recipe) => recipe.yield === portion);
+
+    if (recipes.length > 0) response.send(recipes);
+    else response.status(404).send('Sorry, we cannot find that!');
+});
+};
+
 app.get('/recipe/:index', whenIncomingRequestIndex);
+app.get('/yield/:portion',whenIncomingRequestYield);
 app.listen(PORT);
