2493c473eec22029a238153ea8ef1e57d4267d97
Goh Yong Siang <gysiang@gmail.com>
2022-02-08 00:16:39 +0800

    get recipe by label completed.

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const PORT = 3004;
 
 const app = express();
 
+/**
+ * http://localhost:3004/recipe/0
+ */
+
 const whenIncomingRequestIndex = (req, res) => {
   console.log('request came in ');
 
@@ -25,6 +29,10 @@ const whenIncomingRequestIndex = (req, res) => {
   });
 };
 
+/**
+ * http://localhost:3004/yield/1
+ */
+
 const whenIncomingRequestYield = (request, response) => {
   console.log('request came in ');
 
@@ -42,7 +50,30 @@ const whenIncomingRequestYield = (request, response) => {
     else response.status(404).send('Sorry, we cannot find that!');
 });
 };
+/** Example
+ * http://localhost:3004/recipe-label/udon-noodle-soup
+ */
+const whenIncomingRequestLabel = (request, response) => {
+  console.log('request came in ');
+
+   read('data.json', (data, error) => {
+
+    if (error) {
+      console.log('read error', error);
+    }
+
+    const kebabCase = string => string.replace(/([a-z])([A-Z])/g, "$1-$2").replace(/[\s_]+/g, '-').toLowerCase();
+
+    const label = request.params.label;
+    console.log(label);
+    const recipes = data.recipes.filter((recipe) => kebabCase(recipe.label) === label);
+
+    if (recipes.length>0) response.send(recipes);
+    else response.status(404).send('Sorry, we cannot find that!');
+});
+};
 
 app.get('/recipe/:index', whenIncomingRequestIndex);
 app.get('/yield/:portion',whenIncomingRequestYield);
+app.get('/recipe-label/:label',whenIncomingRequestLabel);
 app.listen(PORT);
