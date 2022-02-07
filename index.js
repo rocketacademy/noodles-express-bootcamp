e1bce3adaaa7b18752107c20c37794737c5d41ca
kennethongcs <kenneth.ongcs@gmail.com>
2022-02-07 11:22:22 +0800

    base

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hello world');
+import express from 'express';
+import { read } from './jsonFileStorage.js';
+
+const app = express();
+
+app.get('/recipe/:index', (req, res) => {
+  // get index number from browser req
+  const index = req.params.index;
+  read('data.json', (err, content) => {
+    if (err) {
+      console.log('Read Error', err);
+    }
+    if (content.recipes[index]) {
+      res.send(content.recipes[index]);
+    } else {
+      res.status(404).send('We cannot find that.');
+    }
+  });
+});
+
+app.listen(3004, () => {
+  console.log(`Listening on port 3004`);
+});
